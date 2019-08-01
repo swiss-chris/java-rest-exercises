00475b4e63f23b24ef11e3ec5110ac5078bdb5f1
Chris Dickinson <chris@dickinson.ch>
2019-08-02 00:09:54 +0100

    same thing but using kotlin

---
+++ src/main/kotlin/com/sainsburys/imagefinder/ImagefinderApplication.kt
@@ -0,0 +1,11 @@
+package com.sainsburys.imagefinder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImagefinderApplication
+
+fun main(args: Array<String>) {
+    runApplication<ImagefinderApplication>(*args)
+}
