4f320140c14ad5f3e3f339a5382ebe32091a31d6
Chris Dickinson <chris@dickinson.ch>
2019-08-04 00:25:25 +0100

    adding query parameter and nullable links.

--- src/main/kotlin/com/sainsburys/imagefinder/controller/ImageController.kt
+++ src/main/kotlin/com/sainsburys/imagefinder/controller/ImageController.kt
@@ -2,25 +2,26 @@ package com.sainsburys.imagefinder.controller
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 
 data class CollectionWrapper(val collection: Collection)
 data class Collection(val items: List<Item>)
-data class Item(val links: List<Link>)
+data class Item(val links: List<Link>?)
 data class Link(val rel: String, val href: String)
 
 @RestController
 class ImageController {
 
-    val images: List<String>
         @RequestMapping(value = ["/images"], method = [RequestMethod.GET], headers = ["Accept=application/json"])
-        get() {
-            val collectionWrapper = RestTemplate().getForObject("https://images-api.nasa.gov/search?q=clouds", CollectionWrapper::class.java)
+        fun get(@RequestParam q: String?): List<String> {
+            val q = if (!q.isNullOrEmpty()) q else "clouds"
+            val collectionWrapper = RestTemplate().getForObject("https://images-api.nasa.gov/search?q=" + q, CollectionWrapper::class.java)
             return collectionWrapper
                     ?.collection
                     ?.items
-                    ?.map { item -> item.links }
+                    ?.map { item -> item.links ?: ArrayList<Link>() }
                     ?.flatten()
                     ?.filter { item -> "preview".equals(item.rel) }
                     ?.map { item -> item.href }
